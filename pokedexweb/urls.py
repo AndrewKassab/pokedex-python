76e60fc04a74858caabb44de1724e443ceac63b0
andrewkassab <andrewkassab@live.com>
2023-08-23 16:20:07 -0700

    added web app to project

--- pokedexweb/urls.py
+++ pokedexweb/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from pokedexweb.views import DetailView
+from pokedexweb.views import DetailView, ListView
 
 urlpatterns = [
+    path('', ListView.as_view(), name='index'),
     path('<int:pk>', DetailView.as_view(), name='detail')
 ]
--- pokedexweb/views.py
+++ pokedexweb/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
 from django.views import generic
 from pokedexapi.models import Pokemon
 
-# Create your views here.
+
+class ListView(generic.ListView):
+    template_name = 'pokedexweb/index.html'
+    model = Pokemon
+
+    def get_queryset(self):
+        return Pokemon.objects.all()
+
+
 class DetailView(generic.DetailView):
     template_name = 'pokedexweb/detail.html'
     model = Pokemon
